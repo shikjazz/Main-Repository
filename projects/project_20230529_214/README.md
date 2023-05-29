a9676c5906dd87f2ec0669467e29e9059e485bf7
shikjazz <sg7039@srmist.edu.in>
2023-05-29 12:00:00 +0530

    Created new project project_20230529_214 on 2023-05-29

---
+++ projects/project_20230529_214/README.md
@@ -0,0 +1,3 @@
+# project_20230529_214
+
+This is an auto-generated project.
---
+++ projects/project_20230529_214/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def main():
+    print('Hello from project_20230529_214!')
+
+if __name__ == '__main__':
+    main()
