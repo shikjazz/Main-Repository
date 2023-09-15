f55607ca55b70600010128d62a920e6d3c979c69
shikjazz <96900337+shikjazz@users.noreply.github.com>
2023-09-15 10:39:15 +0530

    text to speech.py

--- text to speech.py
+++ text to speech.py
@@ -1,27 +1,5 @@
-# Import the required module for text
-# to speech conversion
-from gtts import gTTS
-
-# This module is imported so that we can
-# play the converted audio
-import os
-
-# The text that you want to convert to audio
-mytext = 'booom booom boooom'
-
-
-# Language in which you want to convert
-language = 'en'
-
-# Passing the text and language to the engine,
-# here we have marked slow=False. Which tells
-# the module that the converted audio should
-# have a high speed
-myobj = gTTS(text=mytext, lang=language, slow=False)
-
-# Saving the converted audio in a mp3 file named
-# welcome
-myobj.save("hi.mp3")
-
-# Playing the converted file
-os.system(" hi.mp3")
+import pyttsx3
+text_speech = pyttsx3.init()
+answer = input("What do you want to convert to speech?")
+text_speech.say(answer)
+text_speech.runAndWait()
